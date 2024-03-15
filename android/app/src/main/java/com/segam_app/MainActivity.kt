468a28cc47dcff94aa1807a06b6e078e40184ac3
kmsu44 <kmsu444@gmail.com>
2024-03-15 18:57:03 +0900

    feat: 안드로이드 splashscreen추가

--- android/app/src/main/java/com/segam_app/MainActivity.kt
+++ android/app/src/main/java/com/segam_app/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.segam_app
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import org.devio.rn.splashscreen.SplashScreen;
 
 class MainActivity : ReactActivity() {
 
@@ -11,6 +13,10 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+   override fun onCreate(savedInstanceState: Bundle?) {
+SplashScreen.show(this);
+super.onCreate(null)
+}
   override fun getMainComponentName(): String = "segam_app"
 
   /**
@@ -19,4 +25,5 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+      
 }
